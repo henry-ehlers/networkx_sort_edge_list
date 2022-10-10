0295d29db2a19cf518c5939aa4b25cec38f7cf1a
Henry Ehlers <henry.ward.ehlers@gmail.com>
2022-10-10 15:14:11 +0200

    sorting based on remaining edges as set

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import itertools as it
 
+
 def unlist(nested_list):
     return list(it.chain.from_iterable(nested_list))
 
 
-def has_unique_order(edges, starting_vertex=None):
+def get_valid_starting_vertex(edges: [(int, int)], starting_vertex: int = None):
+    """"""
 
     # Extract a list of vertices from the list of edges
     vertices = unlist(edges)
@@ -27,7 +29,7 @@ def has_unique_order(edges, starting_vertex=None):
     # Count the number of ends in the sequence is only 2, or all vertices must appear exactly twice
     ends = [node for node, count in vertex_counts.items() if count == 1]
     cycles = [node for node, count in vertex_counts.items() if count == 2]
-    assert (len(ends) == 2) or (len(cycles) == len(vertex_counts)),\
+    assert (len(ends) == 2) or (len(cycles) == len(vertex_counts)), \
         f"Edge sequence has too many or too few end vertices, as nodes {ends} all only appear once, or the number" \
         f"of double matched vertices, {len(cycles)} is not the length of the available nodes {len(vertex_counts)}"
 
@@ -49,26 +51,63 @@ def has_unique_order(edges, starting_vertex=None):
         return vertices[0]
 
 
-def get_ordered_edges(edges, starting_vertex=None):
+def get_first_edge(edges: [(int, int)], starting_vertex: int):
 
-    edges = [tuple(edge) for edge in edges]  # TODO: length differs for cycle vs sequence?
+    # Iterate over all edges in set
+    for edge in edges:
 
-    #
-    sorted_edges = [(None, None)] * len(edges)
-    sorted_edges[0] = edges[0]
-    visited_edges = [{edges[0]}]
+        # Check whether first vertex of edge is the starting vertex
+        if edge[0] == starting_vertex:
+            return edge
 
-    #
-    for i in range(1, len(edges)):
-        for edge in edges:
-            if {edge} in visited_edges:
-                continue
-            if edge[0] == sorted_edges[i-1][1] and (edge[0], edge[1] not in sorted_edges):
-                sorted_edges[i] = edge
-                visited_edges.append({edge})
-            elif edge[1] == sorted_edges[i-1][1] and (edge[1], edge[0] not in sorted_edges):
-                sorted_edges[i] = (edge[1], edge[0])
-                visited_edges.append({edge})
+        # Check whether second vertex of edge is the starting vertex
+        elif edge[1] == starting_vertex:
+            return edge[1], edge[0]
+
+    # Provided starting vertex Mapped to no edge
+    assert True, f"Starting Vertex {starting_vertex} mapped to no edge {edges}"
+
+
+def get_ordered_edges(edges: [(int, int)], starting_vertex: int = None):
+
+    # Get a valid starting vertex / Ensure the provided one is valid
+    starting_vertex = get_valid_starting_vertex(edges, starting_vertex)
+
+    # Convert list of tuples to list of frozen-sets
+    remaining_edges = [frozenset(edge) for edge in edges]
+
+    # Obtain an edge which features the starting vertex
+    first_edge = get_first_edge(edges, starting_vertex)
+
+    # Initialize the list of sorted edges, and remove first one from the remaining set
+    sorted_edges = [first_edge] + [(None, None)] * (len(edges) - 1)
+    remaining_edges.remove(frozenset(first_edge))
+
+    # Iterate over each remaining index to be sorted
+    for i in range(1, len(sorted_edges)):
+
+        # Iterate over all remaining edges to be included
+        for edge_set in remaining_edges:
+
+            # Convert Set Edge to forward and 'reversed' edge
+            f_edge = tuple(edge_set)
+            r_edge = f_edge[::-1]
+
+            # Check whether the forward facing edge matches the last sorted edge
+            if f_edge[0] == sorted_edges[i - 1][1]:
+                sorted_edges[i] = f_edge
+                remaining_edges.remove(frozenset(f_edge))
+                break
+
+            # Check whether the 'reversed' facing edge matches the last sorted edge
+            elif r_edge[0] == sorted_edges[i - 1][1]:
+                sorted_edges[i] = r_edge
+                remaining_edges.remove(frozenset(r_edge))
+                break
+
+    # Ensure
+    assert len(remaining_edges) == 0, \
+        f"Not all edges sorted. These remain: {sorted_edges}"
 
     #
     return sorted_edges
@@ -80,11 +119,7 @@ if __name__ == '__main__':
     edge_list_cyclical = [(1, 2), (5, 2), (1, 0), (5, 0)]
     edge_list_illegals = [(1, 2), (5, 2), (1, 0), (5, 1)]
 
-    print(f"Sequence with no starting Vertex -> {has_unique_order(edge_list_sequence)}")
-    print(f"Sequence with starting Vertex {4} -> {has_unique_order(edge_list_sequence, starting_vertex=4)}")
-    print(f"Cycle with no starting vertex -> {has_unique_order(edge_list_cyclical)}")
-    print(f"Cycle with starting vertex {5} -> {has_unique_order(edge_list_cyclical, starting_vertex=5)}")
-
-    print(f"Sequence with illegal starting Vertex {1} -> {has_unique_order(edge_list_sequence, starting_vertex=1)}")
-    # print(f"Cycle with illegal starting vertex {6} -> {has_unique_order(edge_list_cyclical, starting_vertex=6)}")
-    #print(f"Illegal Edge Set with no starting vertex -> {has_unique_order(edge_list_illegals)}")
+    print(f"Sorted Sequence: {get_ordered_edges(edge_list_sequence)}")
+    print(f"Sorted Sequence with starting vertex {4}: {get_ordered_edges(edge_list_sequence, 4)}")
+    print(f"Sorted Cycle: {get_ordered_edges(edge_list_cyclical)}")
+    print(f"Sorted Cycle with starting vertex {5}: {get_ordered_edges(edge_list_cyclical, 5)}")
